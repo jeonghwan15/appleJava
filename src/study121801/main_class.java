0c05c4af5c91fb117643b055c2ebcb73d29f9416
jeonghwan15 <sdf118@naver.com>
2023-12-18 10:22:13 +0900

    1218

--- src/study121801/main_class.java
+++ src/study121801/main_class.java
@@ -3,7 +3,13 @@ package study121801;
 public class main_class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// 자바스크립트 - let a = new array( );
+		//			  let b = [10,20,30,40];
+		//			  a.push(3123);   b.push(233);
+					  
+	    int arr1 = {101,20};
+		
 
 	}
 
