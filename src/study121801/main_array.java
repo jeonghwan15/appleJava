2337daadd370becdd6b05b21c32443036ab3d769
jeonghwan15 <sdf118@naver.com>
2023-12-18 15:55:13 +0900

    1218

--- src/study121801/main_array.java
+++ src/study121801/main_array.java
@@ -4,8 +4,11 @@
 package study121801;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class main_array {
+	
+	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 	
@@ -29,7 +32,7 @@ public class main_array {
 	//	System.out.println( Arrays.toString(number) );
 		
 		// number 배열을 한줄에 4개씩 4줄로 출력하세요
-		System.out.println("┌──");
+		System.out.println();
 		for( int i=0; i<number.length; i++) {
 			if(i%4==0)
 				System.out.println();
@@ -42,13 +45,67 @@ public class main_array {
 			arr2[1][0] = 50;
 			arr2[1][1] = 10;
 			
-			int[][] bingo = new int[][4] {
+			int[][] bingo = new int[][] {
 				{24,12,45,11} ,{9,4,12,31} , {5,41,39,17}, {1,50,43,22}
 			};
-			 for ( int i=0; i<bingo.lenghth; i++) {
-				 for(int k=0; k<bingo[i].length; k++)
-					 system.out.println();
+		//	 for ( int i=0; i<bingo.length; i++) {
+		//		 for(int k=0; k<bingo[i].length; k++) {
+		//			 if( bingo[i][k] <= 25 )
+		//				 System.out.printf("%2s", "★ ");
+				 }
+		//		 else("%2d" , bingo[i][k])
+	//					 System.out.printf();
+			}
+			 
+			 // bingo 배열에서 25이하는 숫자대신 ★을 출력하시오
+			 
+			 // 크기가 서로 다른 이차원배열
+			 
+			 // int[] a, b, c;
+			 
+			 int[][] arr5 = new int[3][];
+			 int[] a= new int[] {1,2,3,4,5};
+			 int[] b= {10,20,30};
+			 int[] c= {11,22,33,44,55,66,77};
+					 
+			arr5[0] = a;
+			arr5[1] = b;
+			arr5[2] = c;
+			
+			for(int i=0; i<arr5.length; i++) {
+				for(int k=0; k< arr5[i].length; k++) {
+					System.out.print(arr5[i][k]+" ");
+				}
+				System.out.println();
+		}
+		// 자바스크립트 - 배열에 정수나 실수나 문자열을 복합적으로 저장할 수 있다.
+		// 자바 - 배열생성시 부여한 데이터 타입에 맞는 데이터만 저장 가능
+		
+		int num11 = scan.nextInt();
+		String word = scan.nextLine();
+		
+		System.out.println( num11 );
+		
+		String[][] info = new String[3][3];
+		for(int i=0; i<info.length; i++) {
+			System.out.println("이름 : ");
+		info[i][0]= scan.nextline();
+		System.out.print("나이 : ");
+		int age = scan.nextInt();
+		info[i][1] =Integer.toString(age); // 숫자를 문자열로 변경
+					// 문자열을 숫자로 변경 - Integer.parseInt();
+					// Float.parseFloat(); Double.paseDouble();
+		scan.nextLine();
+		System.out.print("직업 : ");
+		info[i][2] = scan.nextLine();
+		}
+		for( int i=0; i< info.length; i++) {
+			for ( int k=0; k < info[i].length; k++) {
+				
+				System.out.printf("이름 : %s 나이 : %s 직업 : %s \n",
+					info[i][k], info[i][k], info[i][k]);
 			}
 		}
-	}
+		// 1차원 배열만 잘 자둘 수 있으면 된다.
+		// 구롬 자연스레 2차원배열도 된다.
 }
