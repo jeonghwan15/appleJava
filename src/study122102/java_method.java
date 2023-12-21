61e1dd24b606f2cccc8a048c75ce4b662df1ee89
jeonghwan15 <sdf118@naver.com>
2023-12-21 12:11:39 +0900

    1221

---
+++ src/study122102/java_method.java
@@ -0,0 +1,11 @@
+package study122102;
+
+public class java_method {
+
+	public static void main(String[] args) {
+		
+		music[] mylist = new music[10];
+		new music("폴킴","있잖아",328), new music(""),
+	}
+
+}
