14f95a1395fcfd5c519fa798164fbb3a605e6590
jeonghwan15 <sdf118@naver.com>
2023-12-22 09:57:52 +0900

    1221

--- src/study122102/java_method.java
+++ src/study122102/java_method.java
@@ -2,7 +2,7 @@ package study122102;
 
 import java.util.Scanner;
 
-public class java_method {1
+public class java_method {
 
 	static void list_print( music[] mylist) {
 		for( music obj : mylist) {
