7544eeada933842eec7fe19727c4bb49bebca6bf
jeonghwan15 <sdf118@naver.com>
2023-12-28 09:19:23 +0900

    1228

---
+++ java_inherit/src/java_interface/java_study.java
@@ -0,0 +1,21 @@
+package java_interface;
+
+public class java_study {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+// 인터페이스
+//	  - 추상클래스의 일종
+//	  - interface 이름
+//	  - 변수를 가질수 없다, 메서드 구현이 불가
+//	  - 기본적으로 메서드는 추상 메서드이다.
+//	  - static 변수, 메서드는 생성가능
+//    - final 변수 ( 상수 )
+
+//    - 클래스들이 구현해야 하는 동작( 메서드 )를 지정 하는 데 사용
